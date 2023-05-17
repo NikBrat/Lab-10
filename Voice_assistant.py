96b0a76ceca6239b4eaa424e62e248b8bfba5ff9
“NIKBRA” <nikbratushka@gmail.com>
2023-05-18 01:23:57 +0300

    More progress

--- Voice_assistant.py
+++ Voice_assistant.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
+import time
+import requests
 
 engine = pyttsx3.init()
-newVoiceRate = 180
+newVoiceRate = 176
 
 voices = engine.getProperty('voices')
 engine.setProperty('voices', 'en')
@@ -13,11 +15,14 @@ for voice in voices:
         engine.setProperty('rate', newVoiceRate)
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = r.listen()
 
 def greetings():
-    engine.say('Hello, I am personal assistant in learning new about every word')
+    engine.say('Hello, My name is NIKRA, I am your personal assistant.')
+    engine.say('My goal is to help you find everything about every word')
+    engine.say('I can open a webpage of the word on the WikiDictionary, play the proper pronunciation.')
+    engine.say('tell you the meaning of the word or give you an example of its usage.' )
+    engine.say('Just tell me: find a word. For example, find a bike, find an apple.' )
+    engine.runAndWait()
 
 
 def talk(say):
@@ -25,3 +30,37 @@ def talk(say):
     engine.runAndWait()
 
 
+def listening():
+    try:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            time.sleep(1.5)
+            print('Listening...')
+            voice = r.listen(source)
+            command = r.recognize_google(voice)
+    except:
+        engine.say("Sorry, I could not recognize what you said.")
+    yield command
+
+
+def asking_commands():
+    try:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            time.sleep(1.5)
+            print('Listening...')
+            voice = r.listen(source)
+            command = r.recognize_google(voice, duration=15)
+    except:
+        pass
+    yield command
+
+
+def NIKRA():
+    speech = listening()
+    if 'find a' in speech:
+        rqst = speech.replace('find a ', '')
+    elif 'find an' in speech:
+        rqst = speech.replace('find a ', '')
+    
+    

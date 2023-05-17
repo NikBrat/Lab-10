2eec23b9fee4e740fc30f55c3dabdc5a0fc0ca25
NikBrat <nikbratushka@gmail.com>
2023-05-17 23:34:55 +0300

    Some progress

--- Voice_assistant.py
+++ Voice_assistant.py
@@ -1,6 +1,27 @@
+import speech_recognition as sr
 import pyttsx3
-from pyttsx3.voice import Voice
 
 engine = pyttsx3.init()
+newVoiceRate = 180
+
 voices = engine.getProperty('voices')
-print('WHAT')
+engine.setProperty('voices', 'en')
+
+for voice in voices:
+    if voice.name == 'Shelley (English (UK))':
+        engine.setProperty('voice', voice.id)
+        engine.setProperty('rate', newVoiceRate)
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen()
+
+def greetings():
+    engine.say('Hello, I am personal assistant in learning new about every word')
+
+
+def talk(say):
+    engine.say(say)
+    engine.runAndWait()
+
+
